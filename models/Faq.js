6ba6e6dafbb560b04a1267d351c3f890428036b3
diellzapp <122102145+diellzapp@users.noreply.github.com>
2024-05-27 23:02:33 +0200

    Updated Faq.js

---
+++ models/Faq.js
@@ -0,0 +1,26 @@
+// models/Faq.js
+
+import mongoose from 'mongoose';
+
+const faqSchema = new mongoose.Schema({
+  question: {
+    type: String,
+    required: true
+  },
+  answer: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Faq = mongoose.model('Faq', faqSchema);
+
+export default Faq;
