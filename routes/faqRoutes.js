97f6df88f06540d687e6757f6d8977b126238d92
diellzapp <122102145+diellzapp@users.noreply.github.com>
2024-05-27 23:04:59 +0200

    Updated faqRoutes.js

---
+++ routes/faqRoutes.js
@@ -0,0 +1,26 @@
+
+import express from 'express';
+
+import {
+  getAllFaqs,
+  getFaqById,
+  createFaq,
+  updateFaq,
+  deleteFaq
+} from '../controllers/faqController.js'; 
+const router = express.Router();
+
+
+router.get('/', getAllFaqs);
+
+router.get('/:id', getFaqById);
+
+router.post('/', createFaq);
+
+
+router.put('/:id', updateFaq);
+
+
+router.delete('/:id', deleteFaq);
+
+export default router;
