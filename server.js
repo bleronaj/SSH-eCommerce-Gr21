dd945bd8b9ce2e2aa9b93fe5e684996e0babed0a
Blina Retkoceri <117458349+blinaretkoceri@users.noreply.github.com>
2024-05-27 18:10:31 +0200

    Updated server.js (#51)

--- server.js
+++ server.js
@@ -1,43 +1,40 @@
-import express from 'express';
-import colors from 'colors';
-import dotenv from 'dotenv';
-import morgan from 'morgan';
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import morgan from "morgan";
 import connectDB from "./config/db.js";
-import authRoutes from './routes/authRoute.js';
-import cors from 'cors';
+import authRoutes from "./routes/authRoute.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import cors from "cors";
+import swaggerApp from "./swaggerConfig.js";
 
-
-//configure env
 dotenv.config();
 
-//rest object
-const app=express();
-
-//database config
 connectDB();
 
-//middlewares
-app.use(cors())
-app.use(express.json())
-app.use(morgan('dev')) 
+const app = express();
 
-//routes
-app.use("/api/v1/auth",authRoutes);
-//database config
-connectDB();
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
 
-//rest api
-app.get("/",(req, res)=>{
-    res.send('<h1>Welcome to ecommerce app</h1>');
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/category", categoryRoutes);
+app.use("/api/v1/product", productRoutes);
+
+// Use Swagger
+app.use(swaggerApp);
+
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome to ecommerce app</h1>");
 });
 
-//PORT
 const PORT = process.env.PORT || 8081;
 
-//run listen
 app.listen(PORT, () => {
-    console.log(`Server running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan.white);
+  console.log(
+    `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
+      .white
+  );
 });
-
-
-
