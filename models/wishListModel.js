356412abc5fc04462923944bf1c68b32bd75229d
Blerona Jashanica <122100952+bleronaj@users.noreply.github.com>
2024-05-27 22:46:16 +0200

    Create wishListModel.js

---
+++ models/wishListModel.js
@@ -0,0 +1,17 @@
+// models/wishlistModel.js
+import mongoose from 'mongoose';
+
+const wishlistSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  products: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Product'
+  }]
+});
+
+const Wishlist = mongoose.model('Wishlist', wishlistSchema);
+export default Wishlist;
