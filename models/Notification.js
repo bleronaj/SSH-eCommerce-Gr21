fd1bc38388355c6b3a3097a76d478ce226668952
diellzapp <122102145+diellzapp@users.noreply.github.com>
2024-05-27 23:58:56 +0200

    Updated Notification.js (#59)

---
+++ models/Notification.js
@@ -0,0 +1,29 @@
+
+import mongoose from 'mongoose';
+
+const notificationSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    read: {
+      type: Boolean,
+      default: false,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Notification = mongoose.model('Notification', notificationSchema);
+
+export default Notification;
